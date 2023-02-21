93331d6c8456bf1aa91e0590cf601c61710fe217
Ji-hoon Lee <anteater1056@gmail.com>
2023-02-21 11:19:33 +0900

    header nav bar scroll

--- src/components/PBody.js
+++ src/components/PBody.js
@@ -1,19 +1,19 @@
 function PBody() {
   return (
     <div>
-      <section>
+      <section id="section-1" style={{ height: 512 }}>
         <h2>Profile</h2>
       </section>
-      <section>
+      <section id="section-2" style={{ height: 512 }}>
         <h2>History</h2>
       </section>
-      <section>
+      <section id="section-3" style={{ height: 512 }}>
         <h2>Skills</h2>
       </section>
-      <section>
+      <section id="section-4" style={{ height: 512 }}>
         <h2>Projects</h2>
       </section>
-      <section>
+      <section id="section-5" style={{ height: 512 }}>
         <h2>Contacts</h2>
       </section>
     </div>
--- src/components/PHeader.js
+++ src/components/PHeader.js
@@ -1,5 +1,42 @@
+import { useCallback } from "react";
+
 function PHeader() {
-  return <header>PlaceHolder - Header</header>;
+  const scrollToSection = useCallback(
+    /**
+     * @param {Event} event
+     */
+    (event) => {
+      event.preventDefault();
+      const section = document.querySelector(
+        `#${event.target.href.split("#")[1]}`
+      );
+
+      section.scrollIntoView({ behavior: "smooth" });
+    },
+    []
+  );
+
+  return (
+    <header id="site-header">
+      <nav id="site-nav-bar">
+        <a onClick={scrollToSection} href="#section-1">
+          Profile
+        </a>
+        <a onClick={scrollToSection} href="#section-2">
+          Records
+        </a>
+        <a onClick={scrollToSection} href="#section-3">
+          Skills
+        </a>
+        <a onClick={scrollToSection} href="#section-4">
+          Projects
+        </a>
+        <a onClick={scrollToSection} href="#section-5">
+          Contacts
+        </a>
+      </nav>
+    </header>
+  );
 }
 
 export default PHeader;
