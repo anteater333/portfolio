3f1bd400952d6ab0c8999b08695ad26e426ce8e6
Ji-hoon Lee <anteater1056@gmail.com>
2023-02-28 10:10:59 +0900

    Tailwind css 기본 색상 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      white: "#f2f2f2",
+      black: "#222222",
+    },
   },
   plugins: [],
 };
