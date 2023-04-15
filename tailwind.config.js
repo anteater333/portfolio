e8d36da911644efc2a9b6350a9348f8712eaa91a
Ji-hoon Lee <anteater1056@gmail.com>
2023-04-15 22:07:53 +0900

    권장 너비-높이 설정
    1차적으로 해당 크기에 맞춰서 페이지를 완성시키는 것이 목표

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      white: "#f2f2f2",
-      black: "#222222",
+    extend: {
+      width: {
+        recommended: "1920px",
+      },
+      height: {
+        recommended: "1200px",
+      },
+      colors: {
+        white: "#f2f2f2",
+        black: "#222222",
+      },
     },
   },
   plugins: [],
