83eeb4d108808788ac633c6c2d2d7d1965d37560
Ji-hoon Lee <anteater1056@gmail.com>
2023-04-25 20:50:21 +0900

    Layout - Footer 미리 만들어버림
    어정쩡하게 멍때리고 있느니

--- src/components/PFooter.tsx
+++ src/components/PFooter.tsx
@@ -1,5 +1,23 @@
 function PFooter() {
-  return <footer className="text-neutral-500">PlaceHolder - Footer</footer>;
+  return (
+    <footer className="flex flex-col text-center text-neutral-500">
+      <div className="mt-10 text-2xl">
+        이 사이트는 React, Tailwind CSS를 사용해 만들어졌습니다.
+      </div>
+      <div className="mt-1">정보 갱신일 : {"2023-04-00"}</div>
+      <div className="my-8">
+        Copyright© 2023.{" "}
+        <a
+          className="cursor-pointer"
+          href="https://github.com/anteater333/portfolio"
+          target="blank"
+        >
+          anteater333
+        </a>{" "}
+        All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
 export default PFooter;
