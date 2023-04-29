9611834eb64f6ecec413173a3fba600190d7c7d4
Ji-hoon Lee <anteater1056@gmail.com>
2023-04-29 17:32:49 +0900

    Feat - 스크롤에 맞춰 레코드 데이터 표시 애니메이션 적용

--- src/components/sections/RecordsSection.tsx
+++ src/components/sections/RecordsSection.tsx
@@ -7,11 +7,15 @@ import imgReflection from "../../resources/images/records/img_s2_01_reflection.p
 import imgTVNoise from "../../resources/images/records/img_s2_02_tv_noise.gif";
 import useIntersection from "../../hooks/useIntersection";
 
+import { easeInBack, easeOutBack } from "../../utils/easeFunctions";
+
 type RecordSpecItemProp = { text: string; emoji?: string; big?: boolean };
 
 function RecordSpecItem({ text, emoji, big }: RecordSpecItemProp) {
   return (
-    <span className={big ? "text-5xl" : ""}>#{text.replaceAll(" ", "_")}</span>
+    <span key={text} className={big ? "text-5xl" : ""}>
+      #{text.replaceAll(" ", "_")}
+    </span>
   );
 }
 
@@ -110,9 +114,9 @@ const recordsArray: {
 /** 레코드 수 */
 const numberOfRecords = recordsArray.length;
 /** 레코드 당 스크롤 길이 */
-const scrollPerRecord = 2000;
+const scrollPerRecord = 3000;
 /** 다음 레코드로 넘어가는 것을 미리 알리는 스크롤 임계값 */
-const threshold = 1200;
+const threshold = 2500;
 
 function RecordsSection({ updateLoadingProgress }: SectionProps) {
   const {
@@ -123,6 +127,9 @@ function RecordsSection({ updateLoadingProgress }: SectionProps) {
   const ImgTVNoise = useImageLoader(imgTVNoise);
 
   const [recordScroll, setRecordScroll] = useState(0);
+  const [calcedRecordScroll, setCalcedRecordScroll] = useState(
+    recordScroll % scrollPerRecord
+  );
 
   const ref = useRef<HTMLDivElement | null>(null);
   const entry = useIntersection(ref, {});
@@ -145,21 +152,76 @@ function RecordsSection({ updateLoadingProgress }: SectionProps) {
     []
   );
 
+  /**
+   * 현재 표시될 Record
+   */
+  const [currentRecord, setCurrentRecord] = useState(0);
+
   /** 사진 영역에 노이즈 GIF 표시 여부 */
   const [isNoiseOn, setIsNoiseOn] = useState(false);
   const [recordIndex, setRecordIndex] = useState(0);
 
   useEffect(() => {
-    setIsNoiseOn(recordScroll % scrollPerRecord > threshold); // 일정 threshold 이상 넘어가면 노이즈 표시
+    const calculated = recordScroll % scrollPerRecord;
+    setIsNoiseOn(calculated > threshold); // 일정 threshold 이상 넘어가면 노이즈 표시
     setRecordIndex(
-      Math.min(Math.floor(recordScroll / scrollPerRecord), numberOfRecords - 1)
+      Math.min(Math.floor(recordScroll / scrollPerRecord), numberOfRecords - 2)
     );
+    setCalcedRecordScroll(calculated);
   }, [recordScroll]);
 
+  const [recordPosition, setRecordPosition] = useState(0);
+  const [subRecordPosition, setSubRecordPosition] = useState(0);
+
   useEffect(() => {
-    console.log(recordIndex);
-    console.log(recordsArray[6]);
+    setCurrentRecord(Math.min(recordIndex, numberOfRecords - 3));
   }, [recordIndex]);
+
+  /** 좌측 메인 Record 위치 변경 */
+  useEffect(() => {
+    if (
+      recordIndex !== 0 &&
+      calcedRecordScroll < 1000 &&
+      recordIndex < numberOfRecords - 2
+    ) {
+      // 밑에서 본위치로 (from 400)
+      setRecordPosition(400 - 400 * easeOutBack(calcedRecordScroll / 1000));
+    } else if (
+      recordIndex < numberOfRecords - 3 &&
+      calcedRecordScroll >= 2000
+    ) {
+      // 본위치에서 위로 (to -400)
+      setRecordPosition(
+        0 - 400 * easeInBack((calcedRecordScroll - 2000) / 1000)
+      );
+    } else {
+      setRecordPosition(0);
+    }
+  }, [calcedRecordScroll, recordIndex]);
+
+  /** 우측 서브 Record 위치 변경 */
+  useEffect(() => {
+    if (
+      recordIndex !== 0 &&
+      calcedRecordScroll < 1300 &&
+      recordIndex < numberOfRecords - 2
+    ) {
+      // 밑에서 본위치로 (from 800)
+      setSubRecordPosition(
+        800 - 800 * easeOutBack((calcedRecordScroll - 300) / 1000)
+      );
+    } else if (
+      recordIndex < numberOfRecords - 3 &&
+      calcedRecordScroll >= 2300
+    ) {
+      // 본위치에서 위로 (to -800)
+      setSubRecordPosition(
+        0 - 800 * easeInBack((calcedRecordScroll - 2300) / 700)
+      );
+    } else {
+      setSubRecordPosition(0);
+    }
+  }, [calcedRecordScroll, recordIndex]);
 
   return (
     <section
@@ -170,9 +232,9 @@ function RecordsSection({ updateLoadingProgress }: SectionProps) {
       onScroll={calcCurrentPosition}
       ref={ref}
     >
-      <div className="-scroll-pb-0 h-[14000px] -scroll-mt-6">
+      <div className="-scroll-pb-0 h-[22000px] -scroll-mt-6">
         <div className="sticky top-0 h-recommended">
-          <h1 className="absolute right-16 top-40 border-b-[1rem] border-b-blue-500 text-10xl font-bold leading-[10rem] text-blue-500">
+          <h1 className="absolute right-16 top-40 z-50 border-b-[1rem] border-b-blue-500 text-10xl font-bold leading-[10rem] text-blue-500">
             Records
           </h1>
           <div
@@ -181,60 +243,66 @@ function RecordsSection({ updateLoadingProgress }: SectionProps) {
           >
             <div id="records-content-left" className="w-2/3">
               <div className="flex h-full w-full flex-col pl-28 pt-36">
-                <div
-                  id="records-picture-area"
-                  className="flex h-[27.5rem] w-[27.5rem] justify-center overflow-hidden rounded-[6.25rem] bg-gradient-to-t from-sky-400 to-blue-500"
-                >
-                  <ImgMeCharacter alt="me-character" />
-                  <ImgTVNoise.ImageComponent
-                    className={`${"absolute h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[6.25rem] transition-opacity"} ${
-                      isNoiseOn ? "opacity-100" : "opacity-0"
-                    }`}
-                    alt="tv-noise"
-                  />
-                  <div className="absolute h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[6.25rem] border-[24px] border-black bg-transparent">
-                    <img
-                      className="absolute opacity-75"
-                      src={imgReflection}
-                      alt="records-reflection"
-                      style={{
-                        bottom:
-                          650 *
-                            Math.tanh(
-                              (recordScroll % scrollPerRecord) / 500 - 1
-                            ) -
-                          100,
-                      }}
-                    />
-                  </div>
+                <div className="z-20 flex w-full justify-start bg-white">
                   <div
-                    className="absolute h-[27.5rem] w-[27.5rem] rounded-[6.25rem]"
-                    style={{
-                      background: "radial-gradient(#00000000 60%, #00000099)",
-                    }}
-                  ></div>
+                    id="records-picture-area"
+                    className="flex h-[27.5rem] w-[27.5rem] justify-center overflow-hidden rounded-[6.25rem] bg-gradient-to-t from-sky-400 to-blue-500"
+                  >
+                    <ImgMeCharacter alt="me-character" />
+                    <ImgTVNoise.ImageComponent
+                      className={`${"absolute h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[6.25rem] transition-opacity"} ${
+                        isNoiseOn || recordIndex > numberOfRecords - 3
+                          ? "opacity-100"
+                          : "opacity-0"
+                      }`}
+                      alt="tv-noise"
+                    />
+                    <div className="absolute h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[6.25rem] border-[24px] border-black bg-transparent">
+                      <img
+                        className="absolute opacity-75"
+                        src={imgReflection}
+                        alt="records-reflection"
+                        style={{
+                          bottom:
+                            650 * Math.tanh(calcedRecordScroll / 1200 - 1) -
+                            100,
+                        }}
+                      />
+                    </div>
+                    <div
+                      className="absolute h-[27.5rem] w-[27.5rem] rounded-[6.25rem]"
+                      style={{
+                        background: "radial-gradient(#00000000 60%, #00000099)",
+                      }}
+                    ></div>
+                  </div>
                 </div>
                 <div className="h-full flex-1">
                   <div
-                    className="transition-{transform} absolute -ml-4 flex flex-col text-5xl delay-0 duration-500 ease-[cubic-bezier(0.68,-0.6,0.32,1.0)]"
+                    className="absolute -ml-4 flex flex-col text-5xl"
                     style={{
-                      transform: isVisible
-                        ? "translateY(0)"
-                        : "translateY(450px)",
+                      transform: `translateY(${recordPosition}px)`,
                     }}
                   >
                     <div className="flex space-x-6 pt-6">
                       <div className="flex w-44 items-center justify-end text-[4rem]">
-                        <span>{recordsArray[recordIndex].year}</span>
+                        <span>{recordsArray[currentRecord].year}</span>
                       </div>
                       <div className="h-[4.5rem] w-[4.5rem] rounded-full border-[1rem] border-black bg-white"></div>
                       <div className="flex items-center justify-center">
-                        <span>{recordsArray[recordIndex].title}</span>
+                        <span>{recordsArray[currentRecord].title}</span>
                       </div>
                     </div>
                     <div className="mt-4 flex h-64 flex-col justify-center space-y-8 pl-[18.5rem] text-4xl">
-                      {recordsArray[recordIndex].lines.map((line) => {
-                        return <span className="space-x-4">{line}</span>;
+                      {recordsArray[currentRecord].lines.map((line, index) => {
+                        return (
+                          <span
+                            key={`record-context-${currentRecord}-${index}`}
+                            className="space-x-4"
+                          >
+                            {line}
+                          </span>
+                        );
                       })}
                     </div>
                   </div>
@@ -246,46 +314,43 @@ function RecordsSection({ updateLoadingProgress }: SectionProps) {
               id="records-content-right"
               className="flex h-full w-1/3 flex-col justify-end font-bold"
             >
-              <div className="h-[37.5rem]">
+              <div className="z-30 h-full flex-grow-0 bg-white"></div>
+              <div className="mt-6 h-[37.5rem] flex-shrink-0">
                 <div className="absolute flex flex-col space-y-28">
                   {/* Next Data #1 */}
-                  {recordIndex < numberOfRecords - 1 ? (
+                  {currentRecord < numberOfRecords - 1 ? (
                     <div
-                      className="transition-{transform} flex space-x-6 pt-[3.75rem] delay-100 duration-700 ease-[cubic-bezier(0.68,-0.6,0.32,1.5)]"
+                      className="flex space-x-6 pt-[3.75rem]"
                       style={{
-                        transform: isVisible
-                          ? "translateY(0)"
-                          : "translateY(550px)",
+                        transform: `translateY(${subRecordPosition}px)`,
                       }}
                     >
                       <div className="h-[4.5rem] w-[4.5rem] rounded-full border-[1rem] border-black bg-white"></div>
                       <div>
                         <div className="flex w-44 items-center text-5xl text-[4rem]">
-                          <span>{recordsArray[recordIndex + 1].year}</span>
+                          <span>{recordsArray[currentRecord + 1].year}</span>
                         </div>
                         <div className="mt-2 flex items-center text-4xl">
-                          <span>{recordsArray[recordIndex + 1].title}</span>
+                          <span>{recordsArray[currentRecord + 1].title}</span>
                         </div>
                       </div>
                     </div>
                   ) : undefined}
                   {/* Next Data #2 */}
-                  {recordIndex < numberOfRecords - 2 ? (
+                  {currentRecord < numberOfRecords - 2 ? (
                     <div
-                      className="transition-{transform} flex space-x-6 pt-[3.75rem] delay-200 duration-700 ease-[cubic-bezier(0.68,-0.6,0.32,1.5)]"
+                      className="flex space-x-6 pt-[3.75rem]"
                       style={{
-                        transform: isVisible
-                          ? "translateY(0)"
-                          : "translateY(550px)",
+                        transform: `translateY(${subRecordPosition}px)`,
                       }}
                     >
                       <div className="h-[4.5rem] w-[4.5rem] rounded-full border-[1rem] border-black bg-white"></div>
                       <div>
                         <div className="flex w-44 items-center text-5xl text-[4rem]">
-                          <span>{recordsArray[recordIndex + 2].year}</span>
+                          <span>{recordsArray[currentRecord + 2].year}</span>
                         </div>
                         <div className="mt-2 flex items-center text-4xl">
-                          <span>{recordsArray[recordIndex + 2].title}</span>
+                          <span>{recordsArray[currentRecord + 2].title}</span>
                         </div>
                       </div>
                     </div>
