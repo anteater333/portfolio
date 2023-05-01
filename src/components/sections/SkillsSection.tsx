99f84093234a161649e67842fb67720b8ebaf1e0
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-01 23:14:51 +0900

    Feat - 좌우로 무한 스크롤 되는 배경 요소 구현

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -1,6 +1,93 @@
 import { useEffect } from "react";
 import SectionProps from "./SectionProps";
 
+const bgBannerTextArray = [
+  [
+    "MONGO",
+    "PYTHON",
+    "DOCKER",
+    "HTML",
+    "SQL",
+    "CSS",
+    "JS/TS",
+    "NODE",
+    "GIT",
+    "LINUX",
+  ],
+  [
+    "DOCKER",
+    "SQL",
+    "GIT",
+    "PYTHON",
+    "NODE",
+    "LINUX",
+    "CSS",
+    "HTML",
+    "JS/TS",
+    "MONGO",
+  ],
+  [
+    "LINUX",
+    "HTML",
+    "GIT",
+    "SQL",
+    "PYTHON",
+    "JS/TS",
+    "MONGO",
+    "CSS",
+    "NODE",
+    "DOCKER",
+  ],
+  [
+    "SQL",
+    "LINUX",
+    "JS/TS",
+    "GIT",
+    "MONGO",
+    "PYTHON",
+    "HTML",
+    "CSS",
+    "DOCKER",
+    "NODE",
+  ],
+  [
+    "DOCKER",
+    "LINUX",
+    "GIT",
+    "HTML",
+    "MONGO",
+    "JS/TS",
+    "NODE",
+    "PYTHON",
+    "SQL",
+    "CSS",
+  ],
+  [
+    "PYTHON",
+    "DOCKER",
+    "SQL",
+    "LINUX",
+    "MONGO",
+    "CSS",
+    "JS/TS",
+    "GIT",
+    "NODE",
+    "HTML",
+  ],
+  [
+    "NODE",
+    "PYTHON",
+    "HTML",
+    "JS/TS",
+    "DOCKER",
+    "GIT",
+    "LINUX",
+    "CSS",
+    "MONGO",
+    "SQL",
+  ],
+];
+
 function SkillsSection({ updateLoadingProgress }: SectionProps) {
   useEffect(() => {
     updateLoadingProgress(100, 2);
@@ -14,6 +101,46 @@ function SkillsSection({ updateLoadingProgress }: SectionProps) {
       <h1 className="absolute bottom-10 right-16 border-b-[1rem] border-b-white text-10xl font-bold leading-[10rem] text-white">
         Skills
       </h1>
+      <div className="absolute top-1/2 flex -translate-y-1/2 flex-col items-center justify-center gap-8">
+        {bgBannerTextArray.map((line, index) => {
+          const isOdd = index % 2 === 1;
+          return (
+            <div className="relative  h-32 select-none font-galmuri7 text-9xl font-bold text-white opacity-10">
+              <div
+                className="absolute flex"
+                style={{
+                  left: isOdd ? "0px" : "-4128px",
+                }}
+              >
+                <div
+                  className="flex gap-16 px-8 "
+                  style={{
+                    animation: `${
+                      isOdd ? "bannermove-main" : "bannermove-sub"
+                    } 240s linear infinite`,
+                  }}
+                >
+                  {line.map((text) => {
+                    return <span>{text}</span>;
+                  })}
+                </div>
+                <div
+                  className="flex gap-16 px-8"
+                  style={{
+                    animation: `${
+                      isOdd ? "bannermove-main" : "bannermove-sub"
+                    } 240s linear infinite`,
+                  }}
+                >
+                  {line.map((text) => {
+                    return <span>{text}</span>;
+                  })}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
