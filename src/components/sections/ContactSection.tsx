e04c1fb4444df0fbfe709c5f6e3c05c3dcb1e78a
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-07 18:02:14 +0900

    Feat - 이모지 범 ★ 람

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import SectionProps from "./SectionProps";
 
 import imgContacts00Email from "../../resources/images/contacts/img_s5_00_Email.png";
@@ -9,6 +9,8 @@ import imgContacts04Instagram from "../../resources/images/contacts/img_s5_04_In
 import imgContacts05Blog from "../../resources/images/contacts/img_s5_05_blog.png";
 import imgContacts06BMC from "../../resources/images/contacts/img_s5_06_BMC.png";
 import { useImageLoader } from "../../hooks/useImageLoader";
+import PMatter from "../PMatter";
+import useIntersection from "../../hooks/useIntersection";
 
 function ContactsSection({ updateLoadingProgress }: SectionProps) {
   const ImgContactsEmail = useImageLoader(imgContacts00Email);
@@ -44,130 +46,144 @@ function ContactsSection({ updateLoadingProgress }: SectionProps) {
     updateLoadingProgress,
   ]);
 
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersection(ref, { freezeOnceVisible: true });
+  const isVisible = !!entry?.isIntersecting;
+  const [showEgg, setShowEgg] = useState(false);
+
   return (
     <section
       id="contacts-section"
-      className="relative flex h-recommended snap-center flex-col items-center overflow-scroll bg-pureBlack text-white"
+      className="relative h-recommended snap-center overflow-scroll bg-pureBlack text-white"
+      ref={ref}
     >
-      <h1 className="mt-20 border-b-[1rem] border-b-white text-10xl font-bold leading-[10rem] ">
-        Contacts
-      </h1>
+      <div className="relative h-fit w-full">
+        <div className="relative z-10 flex w-full flex-col items-center">
+          <h1 className="mt-20 border-b-[1rem] border-b-white text-10xl font-bold leading-[10rem] ">
+            Contacts
+          </h1>
 
-      <h2 className="mt-32 text-8xl font-bold">이지훈</h2>
-      <h3 className="mt-2 text-6xl font-bold">Lee Ji-hoon, Anteater</h3>
-
-      <h4 className="mt-8 text-4xl">
-        a SW Developer, a FE Engineer, a Programmer
-      </h4>
-
-      {/* 연락처 리스트 */}
-      <div
-        id="contacts-list-container"
-        className="mt-40 flex w-1/2 flex-col justify-center gap-16"
-      >
-        <div id="contacts-email" className="flex items-center">
-          <ImgContactsEmail.ImageComponent
-            className="mr-8 h-16 w-16"
-            alt="email"
-          />
-          <div className="text-4xl">E-mail</div>
-          <div className="flex-grow text-right text-4xl font-light underline">
-            <a href="mailto:anteater1056@gmail.com">anteater1056@gmail.com</a>
+          <button
+            className="h-32 w-32"
+            disabled={showEgg}
+            onClick={() => setShowEgg(true)}
+          ></button>
+          <h2 className="text-8xl font-bold">이지훈</h2>
+          <h3 className="mt-2 text-6xl font-bold">Lee Ji-hoon, Anteater</h3>
+          <h4 className="mt-8 text-4xl">
+            a SW Developer, a FE Engineer, a Programmer
+          </h4>
+          {/* 연락처 리스트 */}
+          <div
+            id="contacts-list-container"
+            className="mt-40 flex w-1/2 flex-col justify-center gap-16"
+          >
+            <div id="contacts-email" className="flex items-center">
+              <ImgContactsEmail.ImageComponent
+                className="mr-8 h-16 w-16"
+                alt="email"
+              />
+              <div className="text-4xl">E-mail</div>
+              <div className="flex-grow text-right text-4xl font-light underline">
+                <a href="mailto:anteater1056@gmail.com">
+                  anteater1056@gmail.com
+                </a>
+              </div>
+            </div>
+            <div id="contacts-phone" className="flex items-center">
+              <ImgContactsPhone.ImageComponent
+                className="mr-8 h-16 w-16"
+                alt="phone"
+              />
+              <div className="text-4xl">Phone</div>
+              <span className="flex-grow text-right text-4xl font-light">
+                010-4174-4150
+              </span>
+            </div>
+            <div id="contacts-github" className="flex items-center">
+              <ImgContactsGithub.ImageComponent
+                className="mr-8 h-16 w-16"
+                alt="github"
+              />
+              <div className="text-4xl">Github</div>
+              <div className="flex-grow text-right text-4xl font-light underline">
+                <a
+                  href="https://github.com/anteater333"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @anteater333
+                </a>
+              </div>
+            </div>
+            <div id="contacts-talk" className="flex items-center">
+              <ImgContactsTalk.ImageComponent
+                className="mr-8 h-16 w-16"
+                alt="talk"
+              />
+              <div className="text-4xl">KakaoTalk</div>
+              <span className="flex-grow text-right text-4xl font-light">
+                @Anteater333
+              </span>
+            </div>
+            <div id="contacts-instagram" className="flex items-center">
+              <ImgContactsInstagram.ImageComponent
+                className="mr-8 h-16 w-16"
+                alt="instagram"
+              />
+              <div className="text-4xl">Instagram</div>
+              <div className="flex-grow text-right text-4xl font-light underline">
+                <a
+                  href="https://www.instagram.com/anteater333/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @anteater333
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div id="contacts-phone" className="flex items-center">
-          <ImgContactsPhone.ImageComponent
-            className="mr-8 h-16 w-16"
-            alt="phone"
-          />
-          <div className="text-4xl">Phone</div>
-          <span className="flex-grow text-right text-4xl font-light">
-            010-4174-4150
-          </span>
-        </div>
-        <div id="contacts-github" className="flex items-center">
-          <ImgContactsGithub.ImageComponent
-            className="mr-8 h-16 w-16"
-            alt="github"
-          />
-          <div className="text-4xl">Github</div>
-          <div className="flex-grow text-right text-4xl font-light underline">
+          {/* 기타 벳지 및 링크들 */}
+          <div className="mt-36">
             <a
-              href="https://github.com/anteater333"
+              href="https://solved.ac/anteater333/"
               target="_blank"
               rel="noreferrer"
             >
-              @anteater333
+              <img
+                className="h-48 w-96"
+                src="http://mazassumnida.wtf/api/v2/generate_badge?boj=anteater333"
+                alt=""
+              />
             </a>
           </div>
-        </div>
-        <div id="contacts-talk" className="flex items-center">
-          <ImgContactsTalk.ImageComponent
-            className="mr-8 h-16 w-16"
-            alt="talk"
-          />
-          <div className="text-4xl">KakaoTalk</div>
-          <span className="flex-grow text-right text-4xl font-light">
-            @Anteater333
-          </span>
-        </div>
-        <div id="contacts-instagram" className="flex items-center">
-          <ImgContactsInstagram.ImageComponent
-            className="mr-8 h-16 w-16"
-            alt="instagram"
-          />
-          <div className="text-4xl">Instagram</div>
-          <div className="flex-grow text-right text-4xl font-light underline">
+          <div className="mt-16">
             <a
-              href="https://www.instagram.com/anteater333/"
+              href="https://www.buymeacoffee.com/anteater333"
               target="_blank"
               rel="noreferrer"
             >
-              @anteater333
+              <span className="mb-2 inline-block w-full text-center font-galmuri7 text-3xl">
+                ☕ Buy me a potion ☕
+              </span>
+              <ImgContactsBMC.ImageComponent
+                className="h-96 w-96 rounded-3xl"
+                alt="buy-me-a-coffee"
+              />
+            </a>
+          </div>
+          <div className="mb-40 mt-24">
+            <a
+              href="https://blog.anteater-lab.link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ImgContactsBlog.ImageComponent alt="blog" />
             </a>
           </div>
         </div>
-      </div>
 
-      {/* 기타 벳지 및 링크들 */}
-      <div className="mt-36">
-        <a
-          href="https://solved.ac/anteater333/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="h-48 w-96"
-            src="http://mazassumnida.wtf/api/v2/generate_badge?boj=anteater333"
-            alt=""
-          />
-        </a>
-      </div>
-
-      <div className="mt-16">
-        <a
-          href="https://www.buymeacoffee.com/anteater333"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span className="mb-2 inline-block w-full text-center font-galmuri7 text-3xl">
-            ☕ Buy me a potion ☕
-          </span>
-          <ImgContactsBMC.ImageComponent
-            className="h-96 w-96 rounded-3xl"
-            alt="buy-me-a-coffee"
-          />
-        </a>
-      </div>
-
-      <div className="mb-40 mt-24">
-        <a
-          href="https://blog.anteater-lab.link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ImgContactsBlog.ImageComponent alt="blog" />
-        </a>
+        {isVisible && showEgg ? <PMatter /> : undefined}
       </div>
     </section>
   );
---
+++ src/resources/images/contacts/emoji/emoji.ts
@@ -0,0 +1,55 @@
+import E00 from "./0.png";
+import E01 from "./1.png";
+import E02 from "./2.png";
+import E03 from "./3.png";
+import E04 from "./4.png";
+import E05 from "./5.png";
+import E06 from "./6.png";
+import E07 from "./7.png";
+import E08 from "./8.png";
+import E09 from "./9.png";
+import E10 from "./10.png";
+import E11 from "./11.png";
+import E12 from "./12.png";
+import E13 from "./13.png";
+import E14 from "./14.png";
+import E15 from "./15.png";
+import E16 from "./16.png";
+import E17 from "./17.png";
+import E18 from "./18.png";
+import E19 from "./19.png";
+import E20 from "./20.png";
+import E21 from "./21.png";
+import E22 from "./22.png";
+import E23 from "./23.png";
+import E24 from "./24.png";
+
+export const emojiArray = [
+  E00,
+  E01,
+  E02,
+  E03,
+  E04,
+  E05,
+  E06,
+  E07,
+  E08,
+  E09,
+  E10,
+  E11,
+  E12,
+  E13,
+  E14,
+  E15,
+  E16,
+  E17,
+  E18,
+  E19,
+  E20,
+  E21,
+  E22,
+  E23,
+  E24,
+];
+
+export const emojiLength = emojiArray.length;
