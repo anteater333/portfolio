074a395ff53ca36a31aab21c72a77587dc494828
anteater@mac <anteater1056@gmail.com>
2024-04-25 13:51:28 +0900

    Feat - 스크롤에 의한 화면 전환을 동적으로 제어

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,28 +1,48 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import PBody from "../components/PBody";
 import PFooter from "../components/PFooter";
 import PHeader from "../components/PHeader";
 import throttle from "../utils/throttle";
 
 function MainScreen() {
+  const mainScreenRef = useRef<HTMLDivElement>(null);
   const [currentSection, setCurrentSection] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
 
-  /** 현재 스크롤 위치에 따라 현재 섹션을 계산 */
-  const selectCurrentSectionByScroll = useCallback(
-    (event: React.WheelEvent<HTMLElement>) => {
-      const scroll = event.currentTarget.scrollTop;
-      let sectionIndex = Math.floor((scroll + 100) / 1000);
-      setCurrentSection(sectionIndex > 0 ? sectionIndex : 0);
+  /** 사용자의 스크롤 입력을 인식해 섹션 전환 */
+  const changeSectionByScroll = useCallback(
+    (event: WheelEvent) => {
+      if (isScrolling) return;
+
+      /** 스크롤 방향에 따라 계산한 다음 섹션 번호 (0 ~ 4) */
+      let nextSection = Math.min(
+        Math.max(0, currentSection + (event.deltaY > 0 ? 1 : -1)),
+        4
+      );
+
+      mainScreenRef.current?.scrollTo({
+        top: nextSection * 1000,
+        behavior: "smooth",
+      });
+
+      setCurrentSection(nextSection);
+      setIsScrolling(true);
     },
-    []
+    [currentSection, isScrolling]
   );
+
+  /** 스크롤에 의한 화면 전환이 중복해서 발동되는 것을 방지 */
+  useEffect(() => {
+    if (isScrolling) setTimeout(() => setIsScrolling(false), 1000);
+  }, [isScrolling]);
 
   return (
     <>
       <div
         id="app-main-screen"
-        className="relative h-recommended w-recommended snap-y snap-mandatory overflow-scroll"
-        onScroll={throttle(selectCurrentSectionByScroll, 10)}
+        className="relative h-recommended w-recommended snap-y snap-mandatory overflow-hidden"
+        onWheel={throttle(changeSectionByScroll, 250)}
+        ref={mainScreenRef}
       >
         <PHeader selected={currentSection} />
         <PBody />
