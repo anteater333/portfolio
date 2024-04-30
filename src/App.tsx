566ed5d5acadcfc0eadb37e415638e483edd8f40
anteater@mac <anteater1056@gmail.com>
2024-04-30 14:29:17 +0900

    Feat - React Query 사용 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import { LoadingIndicatorProvider } from "./hooks/useLoadingIndicator";
 import MainScreen from "./screens/MainScreen";
 import { CurrentSectionProvider } from "./hooks/useCurrentSection";
 import { SectionScrollableProvider } from "./hooks/useSectionScrollable";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <CurrentSectionProvider>
-      <SectionScrollableProvider>
-        <LoadingIndicatorProvider>
-          <MainScreen />
-        </LoadingIndicatorProvider>
-      </SectionScrollableProvider>
-    </CurrentSectionProvider>
+    <QueryClientProvider client={queryClient}>
+      <CurrentSectionProvider>
+        <SectionScrollableProvider>
+          <LoadingIndicatorProvider>
+            <MainScreen />
+          </LoadingIndicatorProvider>
+        </SectionScrollableProvider>
+      </CurrentSectionProvider>
+    </QueryClientProvider>
   );
 }
 
