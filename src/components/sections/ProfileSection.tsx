284cb12d42e47706f2d011a925440db597af5c09
anteater@mac <anteater1056@gmail.com>
2024-05-07 15:34:05 +0900

    Style - Profile Section on-mobile 레이아웃 구현

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -108,12 +108,12 @@ function ProfileSection({
     >
       <div
         id="profile-content-area"
-        className="absolute z-30 flex h-recommended w-recommended"
+        className="absolute z-30 flex h-recommended w-recommended flex-col md:flex-row"
       >
         <div id="profile-myself-area" className="w-1/3">
           <div
             id="profile-my-IRL-picture"
-            className={`absolute h-full overflow-hidden ${
+            className={`absolute hidden h-full overflow-hidden md:block ${
               isAllImgReady ? "opacity-100" : "opacity-0"
             } ${transitionSequence[6]}`}
           >
@@ -129,7 +129,7 @@ function ProfileSection({
           </div>
           <div
             id="profile-my-character"
-            className={`absolute left-[100px] top-[96px] ${
+            className={`hidden justify-center md:absolute md:left-[100px] md:top-[96px] md:block ${
               isAllImgReady ? "opacity-100" : "opacity-0"
             } ${transitionSequence[0]}`}
           >
@@ -145,7 +145,7 @@ function ProfileSection({
         </div>
         <div
           id="profile-text-area"
-          className="flex-1 select-none pl-12 pr-[128px] pt-[96px] text-white"
+          className="w-screen flex-1 select-none pl-4 pr-4 pt-16 text-white md:w-full md:pl-12 md:pr-[128px] md:pt-[96px]"
         >
           <div
             id="profile-text-area-upper"
@@ -153,11 +153,11 @@ function ProfileSection({
               transitionSequence[3]
             }`}
           >
-            <h1 className="flex justify-between pr-10 text-9xl font-bold">
+            <h1 className="flex justify-between gap-2 text-6xl font-bold md:text-9xl">
               <p className="-ml-2">이지훈</p>
               <p>Lee Jihoon</p>
             </h1>
-            <h3 className="flex justify-between pr-10 text-5xl font-bold">
+            <h3 className="flex justify-between text-xl font-bold md:text-5xl">
               <span>anteater1056@gmail.com</span>
               <div className="flex gap-4">
                 <a
@@ -166,7 +166,11 @@ function ProfileSection({
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img className="h-10 w-10" src={imgNTLogo} alt="nt" />
+                  <img
+                    className="h-6 w-6 md:h-10 md:w-10"
+                    src={imgNTLogo}
+                    alt="nt"
+                  />
                 </a>
                 <a
                   className="flex items-center"
@@ -174,11 +178,15 @@ function ProfileSection({
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img className="h-10 w-10" src={imgGHLogo} alt="gh" />
+                  <img
+                    className="h-6 w-6 md:h-10 md:w-10"
+                    src={imgGHLogo}
+                    alt="gh"
+                  />
                 </a>
               </div>
             </h3>
-            <h3 className="mt-8 text-5xl font-bold">
+            <h3 className="mt-8 text-2xl font-bold md:text-5xl">
               is a SW developer, a front-end engineer,
               <br />a programmer and
             </h3>
@@ -189,10 +197,10 @@ function ProfileSection({
               transitionSequence[4]
             }`}
           >
-            <h2 className="mb-4 text-8xl font-bold">
+            <h2 className="mb-4 text-5xl font-bold md:text-8xl">
               <span className={`text-green-500`}>소프트웨어 장인</span> 지망생
             </h2>
-            <h3 className="text-5xl font-bold">
+            <h3 className="text-4xl font-bold md:text-5xl">
               an Aspiring
               <span className={`text-green-500`}> Software Craftsman</span>
             </h3>
@@ -203,7 +211,7 @@ function ProfileSection({
               isAllImgReady ? "opacity-100" : "opacity-0"
             } ${transitionSequence[5]}`}
           >
-            <h2 className="text-8xl font-bold">
+            <h2 className="text-6xl font-bold md:text-8xl">
               장인의 자세로 <br />
               <span className={`text-green-500`}>공상을 현실로</span> 만드는 삶
             </h2>
